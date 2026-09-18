1e8cd2da4e57187a8d2b3dc36b3d27addae0d561
hermes <hermes@Debian13LXC-trixie>
2026-09-18 08:43:40 +0000

    fix: persist cron badge preference

--- static/panels.js
+++ static/panels.js
@@ -13075,7 +13075,7 @@ function _clearCronUnreadMarkers(){
   _cronNewJobIds.clear();
   if(typeof _clearAllCronSessionCompletionUnread==='function') _clearAllCronSessionCompletionUnread();
   updateCronBadge();
-  if($('cronList')) renderCrons();
+  if($('cronList')&&typeof loadCrons==='function') loadCrons();
 }
 
 // Clear cron badge only when all unread jobs have been viewed (not on panel open)
